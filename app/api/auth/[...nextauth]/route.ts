9deabc683180f5184fb83fb5be7eaf8eb9f1c729
Jacob Peake <jepeake@proton.me>
2025-04-21 20:47:27 +0100

    fix build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,4 @@
-import NextAuth, { type NextAuthOptions, Session } from "next-auth";
-import type { JWT } from "next-auth/jwt";
-import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
-import { compare } from "bcryptjs";
+import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
